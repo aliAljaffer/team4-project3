b6a033ba5462caefb6a61f5e8b69e51bf6d41255
Ali Aljaffer <ahjfr@icloud.com>
2025-10-26 15:39:10 +0300

    AKS fully configured

--- terraform/azure/aks/terraform.tf
+++ terraform/azure/aks/terraform.tf
@@ -14,10 +14,12 @@ resource "azurerm_kubernetes_cluster" "aks" {
     vnet_subnet_id       = var.cluster_subnet_id
   }
 
+
   network_profile {
     service_cidr   = "10.2.0.0/16"
     dns_service_ip = "10.2.0.10"
     network_plugin = "azure"
+
   }
 
 
@@ -27,10 +29,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 
 resource "azurerm_kubernetes_cluster_node_pool" "user_node_pool" {
-  name                  = "${var.resource_prefix}unp"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  vm_size               = var.vm_size
-  node_count            = 2
-  auto_scaling_enabled  = true
-  min_count             = 2
+  name                        = "${var.resource_prefix}unp"
+  kubernetes_cluster_id       = azurerm_kubernetes_cluster.aks.id
+  vm_size                     = var.vm_size
+  vnet_subnet_id              = var.cluster_subnet_id
+  node_count                  = 2
+  auto_scaling_enabled        = true
+  min_count                   = 2
+  max_count                   = 3
+  temporary_name_for_rotation = "team4tmpnm"
 }
